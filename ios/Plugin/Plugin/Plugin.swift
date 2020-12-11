d76822cd1602713a5dbd0f857ba8a9b7edf70a33
Ivan Irydchyn <i.irydchyn@tenantcloud.com>
2020-12-11 18:16:56 +0200

    wip: Added ios method for download

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -8,10 +8,59 @@ import Capacitor
 @objc(FileTransfer)
 public class FileTransfer: CAPPlugin {
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    @objc func download(_ call: CAPPluginCall) {
+        let source: String = call.getString("source") ?? ""
+        let target: String = call.getString("target") ?? ""
+
+        if (!source.isEmpty && !target.isEmpty ) {
+            let url = URL(string: source)
+
+            do {
+                try FileTransfer.loadFileAsync(url: url!, target: target) { (path, error) in
+                    print("File downloaded to : \(path!)")
+                    call.success();
+                }
+            } catch {
+                call.error("Download error. " + error.localizedDescription)
+            }
+        }
+    }
+
+    static func loadFileAsync(url: URL, target: String, completion: @escaping (String?, Error?) -> Void) throws {
+        let destinationUrl = URL(string: target)!
+
+        if FileManager().fileExists(atPath: destinationUrl.path) {
+            print("File already exists [\(target)]")
+            try? FileManager.default.removeItem(at: destinationUrl)
+        }
+
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: nil)
+        var request = URLRequest(url: url)
+
+        request.httpMethod = "GET"
+
+        let task = session.dataTask(with: request, completionHandler: {
+            data, response, error in
+
+            if error == nil {
+                if let response = response as? HTTPURLResponse {
+                    if response.statusCode == 200 {
+                        if let data = data {
+                            if let _ = try? data.write(to: destinationUrl, options: Data.WritingOptions.atomic) {
+                                completion(target, error)
+                            } else {
+                                completion(target, error)
+                            }
+                        } else {
+                            completion(target, error)
+
+                        }
+                    }
+                }
+            } else {
+                completion(target, error)
+            }
+        })
+        task.resume()
     }
 }
